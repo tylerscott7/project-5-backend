4402a235c986f633063e3b520b108b8159fe3e6a
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2019-03-03 17:56:55 -0700

    added serializers and routes for users (admin)

--- condue/urls.py
+++ condue/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('reservation.urls')),
     path('', include('review.urls')),
+    path('', include('user.urls')),
 ]
--- review/serializers.py
+++ review/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from review.models import Review
 
-# Reservation Serializer
+# Review Serializer
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review

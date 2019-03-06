3559454dda54e45358e7a7fa8d70422ae3edeb6b
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2019-03-06 11:01:58 -0700

    authentication for users

--- condue/urls.py
+++ condue/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import login, sample_api
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/login', login),
+    path('api/users/sampleapi', sample_api),
     path('', include('reservation.urls')),
     path('', include('review.urls')),
     path('', include('user.urls')),

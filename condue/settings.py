6d2c296d88988a16b620e5da101e9f185a1d55aa
Tyler Nicholson <tylernicholson@Tylers-MacBook-Pro.local>
2019-03-06 12:14:39 -0700

    setting up heroku deployment

--- condue/settings.py
+++ condue/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -92,8 +93,12 @@ WSGI_APPLICATION = 'condue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project5',
+        'USER': 'tylernicholson',
+        'PASSWORD': 'tylernicholson',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -143,3 +148,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated', )
 }
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
